87c87773385d26c72eafb1b3d79a020f71da4e90
ginny <pig@ginny.com>
2017-06-01 07:03:00 +0800

    ..

---
+++ continue.py
@@ -0,0 +1,12 @@
+for letter in 'python':
+	if letter == 'h':
+		continue
+	print('the current letter is:',letter)
+
+var=10
+while var >0:
+	var=var-1
+	if var ==5:
+		continue
+	print('the current variable is:',var)
+print('good bye!')
---
+++ in.py
@@ -0,0 +1,12 @@
+dict={'Name':'Runoob','Age':7}
+
+if 'Age' in dict:
+	print("yes,Age is in dict.")
+else:
+	print("no,Age is not in dict.")
+
+if 'sex' in dict:
+	print("yes,sex is in dict.")
+else:
+	print("no,sex is not in dict.")
+
